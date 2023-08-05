30edca4febedd645711f82ff96c97b8f9f1e46cc
gyan-cell <jha07944@gmail.com>
2023-08-05 11:06:38 +0530

    Arrays In TS

---
+++ arrays.ts
@@ -0,0 +1,15 @@
+const superHeroe = [] // Default Type is Return.. Instead Do this
+
+const superHeroes : string [] = [];
+// Or We Can Do this ..
+superHeroes.push("Superman....");
+const superPowers : Array <number> = []
+
+type user = {
+  name:string,
+  isActive:boolean
+}
+
+const  allUsers : user[] = [];
+allUsers.push({name:"",isActive:true})
+// allUsers.push("")
