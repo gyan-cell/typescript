1464c8cbecbfc3d33a401e87f1924ab8061b3185
gyan-cell <jha07944@gmail.com>
2023-08-09 20:15:08 +0530

    minor changes

--- Objects.ts
+++ Objects.ts
@@ -4,21 +4,19 @@ const user = {
   isActive: true,
 };
 
-function createUser({ name: string, isPaid: boolean }) {
+function createUser({ name: string, isPaid: boolean }) {}
 
-}
-
-createUser({name:"Gyan",isPaid:true});
+createUser({ name: "Gyan", isPaid: true });
 //In These Case I can Only send Information Which is allowed To Export in function;
-function createCourse():{name:string,price:number}{
-  return {name:"Gyan",price:16};
+function createCourse(): { name: string; price: number } {
+  return { name: "Gyan", price: 16 };
 }
 //In These Case I can Only send Information Which is allowed To Export in function;
-function createCourses():{name:string,price:number}{
-  let user = {name:"Gyan",price:16, Email:"hello@ttygmail.com"}
+function createCourses(): { name: string; price: number } {
+  let user = { name: "Gyan", price: 16, Email: "hello@ttygmail.com" };
   return user;
 }
 createCourse();
 createCourses();
 
-export {}
+export {};
